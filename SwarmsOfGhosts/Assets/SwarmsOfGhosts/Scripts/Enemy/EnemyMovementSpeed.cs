b3b95b52b704845de28d023b9f83115832739918
Vladimir Klubkov <vladimir.gamedev@gmail.com>
2023-10-20 03:13:25 +0400

    Add enemy spawner

---
+++ SwarmsOfGhosts/Assets/SwarmsOfGhosts/Scripts/Enemy/EnemyMovementSpeed.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Entities;
+
+namespace SwarmsOfGhosts.Enemy
+{
+    public struct EnemyMovementSpeed : IComponentData
+    {
+        public float Value;
+    }
+}
---
+++ SwarmsOfGhosts/Assets/SwarmsOfGhosts/Scripts/Enemy/EnemySettings.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+namespace SwarmsOfGhosts.Enemy
+{
+    [GenerateAuthoringComponent]
+    public struct EnemySettings : IComponentData
+    {
+        public float2 SpeedRange;
+    }
+}
